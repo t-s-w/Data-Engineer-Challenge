b8c7d76a00e9e42d75a39069379195589288de61
amphipath <55447360+t-s-w@users.noreply.github.com>
2023-04-14 15:29:02 +0800

    add code to generate covid cases over time graph

---
+++ 4 - Charts and APIs/generate_graph.py
@@ -0,0 +1,12 @@
+import requests
+import json
+import pandas as pd
+import plotly.express as px
+
+response_API = requests.get('https://api.covid19api.com/country/singapore/status/confirmed?from=2020-03-01T00:00:00Z&to=2022-03-01T00:00:00Z')
+data = json.loads(response_API.content)
+
+df = pd.DataFrame([{'Date':row['Date'],'Cases':row['Cases']} for row in data])
+
+fig = px.line(df,x = 'Date',y = 'Cases')
+fig.write_html('readme.html')
